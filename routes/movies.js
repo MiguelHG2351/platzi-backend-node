5e6b880a83d24d4d8709346fc467949ecff395e2
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-07-04 10:12:47 -0600

    el servicio se llamaba después de la ruta

--- routes/movies.js
+++ routes/movies.js
@@ -7,11 +7,11 @@ const cacheResponse = require('../utils/cacheResponse')
 const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time')
 
 function moviesApi(app) {
-    const router = express.Router()
+    const movieServices = new MovieServices()
 
+    const router = express.Router()
     app.use('/api/movies', router)
 
-    const movieServices = new MovieServices()
 
     router.get('/', async (req, res, next) => {
         cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
